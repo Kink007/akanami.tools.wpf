6e063e7ce92fe9959d476cf702bb06e4b170ed41
super_wyx@163.com <super_wyx@163.com>
2020-11-10 18:07:32 +0800

    重新更改版面

--- src/Akanami.FastDev/Client/Akanami.FastDev.Client/App.xaml.cs
+++ src/Akanami.FastDev/Client/Akanami.FastDev.Client/App.xaml.cs
@@ -1,45 +1,32 @@
-﻿using Akanami.FastDev.Client.Service;
-using Akanami.FastDev.Client.Service.Impl;
-using Akanami.FastDev.Client.Views;
-using Akanami.FastDev.QuickStart;
+﻿using Akanami.FastDev.Client.Views;
 using Prism.Ioc;
-using Prism.Modularity;
 using Prism.Unity;
-using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using Unity;
 
 namespace Akanami.FastDev.Client
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// App.xaml 的交互逻辑
     /// </summary>
     public partial class App : PrismApplication
     {
         protected override Window CreateShell()
         {
-            return Container.Resolve<MainWindow>();
+            var shell = this.Container.Resolve<MainView>();
+
+            return shell;
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            containerRegistry.Register<ISoftwareInformationService, SoftwareInformationServiceImpl>();
+            
         }
-
-        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
-        {
-            moduleCatalog.AddModule<QuickStartModule>();
-        }
-
-        //protected override IModuleCatalog CreateModuleCatalog()
-        //{
-        //    //var startupPath = Path.GetDirectoryName(typeof(App).Assembly.Location);
-        //    //var moduleDirectory = Path.Combine(startupPath, "modules");
-        //    //if (!Directory.Exists(moduleDirectory))
-        //    //{
-        //    //    Directory.CreateDirectory(moduleDirectory);
-        //    //}
-
-        //    //return new DirectoryModuleCatalog() { ModulePath = moduleDirectory };
-        //}
     }
 }
---
+++ src/Akanami.FastDev/Client/Akanami.FastDev.Client/Views/MainView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Akanami.FastDev.Client.Views
+{
+    /// <summary>
+    /// MainView.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnWindowMinisize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnWindowMaxsize_Click(object sender, RoutedEventArgs e)
+        {
+            this.btnWindowShrink.Visibility = Visibility.Visible;
+            this.btnWindowMaxsize.Visibility = Visibility.Collapsed;
+
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void btnWindowShrink_Click(object sender, RoutedEventArgs e)
+        {
+            this.btnWindowShrink.Visibility = Visibility.Collapsed;
+            this.btnWindowMaxsize.Visibility = Visibility.Visible;
+
+            this.WindowState = WindowState.Normal;
+        }
+
+        private void btnWindowClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
