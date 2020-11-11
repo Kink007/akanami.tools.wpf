9a6c445be1d7c0f13192a533f5ca4c6449dd786b
super_wyx@163.com <super_wyx@163.com>
2020-11-11 09:01:07 +0800

    更新首页样式

---
+++ src/Akanami.FastDev/Client/Akanami.FastDev.Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akanami.FastDev.Client.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        string _title = "快速开发工具";
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                SetProperty(ref _title, value);
+            }
+        }
+    }
+}
